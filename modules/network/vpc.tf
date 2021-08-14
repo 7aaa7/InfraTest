cc20861c34d878e823d5ead8d6cb8a4a14820868
Saurabh Verma <s@3w3.one>
2021-08-14 17:20:48 +0530

    TEST-1: Adding two subnets and removing terraform state from git repo, for this I'm using state inside local directory not a remote state.

--- modules/network/vpc.tf
+++ modules/network/vpc.tf
@@ -17,3 +17,31 @@ resource "aws_vpc" "vpc" {
     CreatedBy = "Terraform"
   }
 }
+
+data "aws_availability_zones" "available" {
+  state = "available"
+}
+
+
+resource "aws_subnet" "subnet_public_1a" {  
+  vpc_id                  = aws_vpc.vpc.id
+  cidr_block              = var.subnet_public_1a_cidr
+  availability_zone       = data.aws_availability_zones.available.names[0]
+  map_public_ip_on_launch = true
+  tags = {
+    Name = "${var.config.name}-subnet-public-1a"
+    Tier = "Public"
+  }
+}
+
+
+resource "aws_subnet" "subnet_backend_1a" {  
+  vpc_id            = aws_vpc.vpc.id
+  cidr_block        = var.subnet_backend_1a_cidr
+  availability_zone = data.aws_availability_zones.available.names[0]
+  tags = {
+    Name = "${var.config.name}-subnet-backend-1a"
+    Tier = "Private"
+    Type = "Backend"
+  }
+}
